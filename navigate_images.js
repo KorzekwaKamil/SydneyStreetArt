ddaab55c41d9dab8d13fed784f2bdb97d17d4390
KorzekwaKamil <31800298+KorzekwaKamil@users.noreply.github.com>
2017-09-10 11:33:06 +1000

    Update navigate_images.js
    Blurring...

--- navigate_images.js
+++ navigate_images.js
@@ -2,6 +2,7 @@ async function next_image() {
 	var fileNameWithOutExt = current_image_name.substring(0,current_image_name.lastIndexOf('.'));
 	var image_handle=document.getElementById('graffiti');
 	let temp=await blur(image_handle);
+	if (temp==0) {
 	if (current_image==number_of_images) {
 		current_image=1;
 		image_handle.src = "Photos\\"+current_image_name;
@@ -14,7 +15,7 @@ async function next_image() {
 		current_image=current_image+1;
 		image_handle.src = "Photos\\"+fileNameWithOutExt+"_"+current_image+".jpg";
 	}
-	image_handle.style.WebkitFilter="blur(0px)";
+	image_handle.style.WebkitFilter="blur(0px)";}
 }
 		
 function previous_image() {
