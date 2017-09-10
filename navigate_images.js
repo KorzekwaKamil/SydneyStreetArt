12032b14b172574c8adbc3aa54150ad7af141b0c
KorzekwaKamil <31800298+KorzekwaKamil@users.noreply.github.com>
2017-09-10 10:33:33 +1000

    Update navigate_images.js
    Added blurring while changing pictures

--- navigate_images.js
+++ navigate_images.js
@@ -1,30 +1,36 @@
 function next_image() {
 	var fileNameWithOutExt = current_image_name.substring(0,current_image_name.lastIndexOf('.'));
+	var image_handle=document.getElementById('graffiti');
+	image_handle.style.WebkitFilter="blur(10px)";
 	if (current_image==number_of_images) {
 		current_image=1;
-		document.getElementById('graffiti').src = "Photos\\"+current_image_name;
+		image_handle.src = "Photos\\"+current_image_name;
 	}
 	else if (current_image<9) {
 		current_image=current_image+1;
-		document.getElementById('graffiti').src = "Photos\\"+fileNameWithOutExt+"_0"+current_image+".jpg";
+		image_handle.src = "Photos\\"+fileNameWithOutExt+"_0"+current_image+".jpg";
 	}
 	else {
 		current_image=current_image+1;
-		document.getElementById('graffiti').src = "Photos\\"+fileNameWithOutExt+"_"+current_image+".jpg";
+		image_handle.src = "Photos\\"+fileNameWithOutExt+"_"+current_image+".jpg";
 	}
+	image_handle.style.WebkitFilter="blur(0px)";
 }
 		
 function previous_image() {
 	var fileNameWithOutExt = current_image_name.substring(0,current_image_name.lastIndexOf('.'));
+	var image_handle=document.getElementById('graffiti');
+	image_handle.style.WebkitFilter="blur(10px)";
 	current_image=current_image-1;
 	if (current_image==0) {current_image=number_of_images;}
 	if (current_image==1) {
-		document.getElementById('graffiti').src = "Photos\\"+fileNameWithOutExt+".jpg";
+		image_handle.src = "Photos\\"+fileNameWithOutExt+".jpg";
 	}
 	else if (current_image<10) {
-		document.getElementById('graffiti').src = "Photos\\"+fileNameWithOutExt+"_0"+current_image+".jpg";
+		image_handle.src = "Photos\\"+fileNameWithOutExt+"_0"+current_image+".jpg";
 	}
 	else {
-		document.getElementById('graffiti').src = "Photos\\"+fileNameWithOutExt+"_"+current_image+".jpg";
+		image_handle.src = "Photos\\"+fileNameWithOutExt+"_"+current_image+".jpg";
 	}
+	image_handle.style.WebkitFilter="blur(0px)";
 }
