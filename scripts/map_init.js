525d87c6f0b6e88bf4e96a3f54d7e53786817c9b
KorzekwaKamil <31800298+KorzekwaKamil@users.noreply.github.com>
2017-09-21 22:59:33 +1000

    Update map_init.js
    Correcting google maps - no more global referencing

--- scripts/map_init.js
+++ scripts/map_init.js
@@ -12,8 +12,6 @@ var current_image=0;
 var current_zoom=12;
 var current_center={lat: -33.8955111419, lng: 151.179684587};
 		
-var map;	
-
 function initMap() {
 	var myLatlng = new google.maps.LatLng(-33.8955111419,151.179684587); 
 	var map_canvas = document.getElementById("map_canvas");
@@ -22,10 +20,12 @@ function initMap() {
 		zoom: current_zoom, 
 		mapTypeId: google.maps.MapTypeId.ROADMAP 
 	}			
-	map = new google.maps.Map(map_canvas, map_options);			
+	var map = new google.maps.Map(map_canvas, map_options);			
 					
 	loadMarkers();
-			
+    document.getElementById("map_group_button").style.visibility="visible";
+    document.getElementById("map_group_button").onclick=group;
+    
 	if (group_markers==1){var markerCluster = new MarkerClusterer(map, all_markers,{imagePath: 'graphics/m'});}
 	  
 	if (first_time_select==0) {
@@ -39,4 +39,82 @@ function initMap() {
 		indicator_marker.setIcon('graphics/big_blue.png');
 		indicator_marker.setVisible(true);
 	}	
+    google.maps.event.addDomListener(window,'resize',function(){google.maps.event.trigger(map, 'resize');});
+	
+	function loadMarkers() {
+		var data_xml=parent.data_xml.responseXML;
+		var graffitis=data_xml.getElementsByTagName("graffiti");
+		previous_path="something_very_strange";
+		image_number=0;
+		for (i = 0; i < graffitis.length; i++) {
+			
+			path=graffitis[i].getElementsByTagName("full_path")[0].childNodes[0].nodeValue;
+			
+			if ( path.substr(0,path.length-7) == previous_path.substr(0,previous_path.length-4)) {
+				image_number++;
+			} 
+			else {
+				if (i>0) {
+					addMarkerToMap(lat,lon,previous_path,i-image_number,image_number);
+				}
+				previous_path=path;
+				lat=graffitis[i].getElementsByTagName("gps_lat")[0].childNodes[0].nodeValue;
+				lon=graffitis[i].getElementsByTagName("gps_lon")[0].childNodes[0].nodeValue;
+				image_number=1;
+			}	
+		}
+		if (image_number>1) {addMarkerToMap(lat,lon,previous_path,graffitis.length-1-image_number,image_number);}
+	}
+
+	function addMarkerToMap(latit, longit, image_name,graffiti_index,image_number){ 
+		all_markers.push(new google.maps.Marker({ position: new google.maps.LatLng(latit, longit),map:map}));
+		all_markers[all_markers.length-1].setIcon('graphics/small_red.png');
+		current_marker=all_markers[all_markers.length-1];
+		marker_count++; 	
+		google.maps.event.addListener(current_marker, 'click', (function(marker, marker_count) { 
+			return function() { 
+				if (first_time_select==0) {
+					first_time_select=1;
+					document.getElementById('info_text').style.visibility="hidden";
+				}
+				document.getElementById('graffiti').style.WebkitFilter="blur(10px)";
+				
+				indicator_marker.setPosition(new google.maps.LatLng(latit, longit));
+				indicator_marker.setVisible(true);
+						
+				current_image=1;
+				number_of_images=image_number;
+				current_graffiti=graffiti_index;
+				if (number_of_images>1) {
+					document.getElementById('next_button').style.visibility="visible";
+					document.getElementById('previous_button').style.visibility="visible";
+				}
+				else {
+					document.getElementById('next_button').style.visibility="hidden";
+					document.getElementById('previous_button').style.visibility="hidden";
+				}
+				load_picture(current_graffiti,current_image);
+			} 
+			})(current_marker, marker_count));
+	}
+    
+    function group() {
+        all_markers=[];
+        marker_count=0;
+        current_position=indicator_marker.getPosition();
+        current_center=map.getCenter();
+        current_zoom=map.getZoom();
+        if (group_markers==1) {
+            group_markers=0;
+            document.getElementById('map_group_button').innerHTML="&nbsp Group &nbsp";
+            initMap();
+        }
+        else {
+            group_markers=1;
+            document.getElementById('map_group_button').innerHTML="Ungroup";
+            initMap();
+        }
+        map.setCenter(current_center);
+        map.setZoom(current_zoom);
+    }
 }
